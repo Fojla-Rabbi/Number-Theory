987b635e5e094671fdfce28e1f3b717ef68c6711
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-11-08 09:56:11 +0600

    Create Sieve of Eratosthenes.cpp

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,33 @@
+// It's just a pre-computation to save all the primes till a number
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 2e5 + 10;
+vector<int> prime;
+void sieve() {
+    vector<int> p(N + 1, 0);
+    for(ll i = 2; i*i <= N; i++) {
+        if(p[i] != 1) {
+            for(ll j = i*i; j <= N; j += i) {
+                p[j] = 1;
+            }
+        }
+    }
+    for(int i = 2; i <= N; i++) {
+        if(p[i] != 1) prime.push_back(i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    sieve();
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
